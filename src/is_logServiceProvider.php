06b6871b59f4a7ccd0757ac0f6dccce1f03572c4
masoudvahidi <50068463+masoudvahidi@users.noreply.github.com>
2019-07-06 10:46:29 +0430

    Update is_logServiceProvider.php

--- src/is_logServiceProvider.php
+++ src/is_logServiceProvider.php
@@ -1,12 +1,22 @@
 <?php
 namespace ishelperlog\is_log;
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 class is_logServiceProvider extends ServiceProvider{
-    public function boot(){
-       // $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    protected $middleware = [
+        'middlewareName' => 'Vendor\mypackage\Http\Middlewares\TerminateMiddleware'
+    ];
+    public function boot(Router $router){
+        parent::boot($router);
+        foreach($this->middleware as $name => $class) {
+            $this->middleware($name, $class);
+        }
 
     }
     public function register(){
-
+        $file = app_path('Helpers/helperfun.php');
+        if (file_exists($file)) {
+            require_once($file);
+        }
     }
 }
